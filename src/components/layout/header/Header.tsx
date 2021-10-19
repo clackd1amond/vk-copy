537924d75f68e4d237a32f4adc96ae6136161899
Eugene <clackd1amond@users.noreply.github.com>
2021-10-19 08:34:41 +0300

    basic structure prepare

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return <div></div>;
+};
+export default Header;
---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+
+const Sidebar: FC = () => {
+  return <div></div>;
+};
+export default Sidebar;
